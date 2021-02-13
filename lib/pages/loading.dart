6118569337262bf300490f9bf855043c3b4da7e9
Jakaria051 <jacjakaria51@gmail.com>
2021-02-13 19:38:53 +0600

    added World Time API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -14,10 +14,14 @@ class _LoadingState extends State<Loading> {
 
    void getData() async{
 
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+     Response response = await get('http://worldtimeapi.org/api/timezone/Europe/London');
+     Map data = jsonDecode(response.body);
+     String datetime = data['datetime'];
+     String offset = data['utc_offset'].substring(1,3);
+     //print(offset);
+     DateTime now = DateTime.parse(datetime);
+     now = now.add(Duration(hours: int.parse(offset)));
+     print(now);
   }
 
   @override
