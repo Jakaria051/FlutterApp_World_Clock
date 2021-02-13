54b0f976a782c4dea46f47d71eba8c8401e5c15b
Jakaria051 <jacjakaria51@gmail.com>
2021-02-13 15:40:21 +0600

    Rest Api is called successfully

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
 
 class Loading extends StatefulWidget {
   Loading({Key key}) : super(key: key);
@@ -8,6 +11,23 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+   void getData() async{
+
+    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+  }
+
+  @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+      print("api will call.................");
+      getData();
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
